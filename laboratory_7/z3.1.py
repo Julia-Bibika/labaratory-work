8120f05f68df5949fc13e481306bf0b2d54af9d2
Anastasiia-Bibika <93943067+Anastasiia-Bibika@users.noreply.github.com>
2021-11-23 22:01:53 +0200

    Update z3.1.py

--- laboratory_7/z3.1.py
+++ laboratory_7/z3.1.py
@@ -4,13 +4,21 @@
 A = []
 i = int(input("Введіть кількість рядків матриці : "))
 j = int(input("Введіть кількість стовбців матриці : "))
+k = 0
+c = 0
 for x in range(i):
     A.append([int(input("Введіть number#{0}{1} : ".format(x, y))) for y in range(j)])
 print(A)
 for x in range(i):
     for y in range(j):
-        if x == y and A[x][y] != 0:
-            print("Матриця A є діагональною матрицею")
+        if x == y:
+            if A[x][y] != 0:
+                k += 1
+                if k == i:
+                    print("Матриця A є діагональною матрицею")
         else:
-            break
-print("Матриця A не є діагональною матрицею")
+            if A[x][y] != 0:
+                c += 1
+                if c == i:
+                    print("Матриця A не є діагональною матрицею")
+                    break

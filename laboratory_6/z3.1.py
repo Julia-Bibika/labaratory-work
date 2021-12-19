c495575698332330f9e8709ab3c4e2d2c3910fde
Anastasiia-Bibika <93943067+Anastasiia-Bibika@users.noreply.github.com>
2021-12-19 18:52:23 +0200

    Update z3.1.py

--- laboratory_6/z3.1.py
+++ laboratory_6/z3.1.py
@@ -1,17 +1,14 @@
+# Завдання 3. Задача 11
+# Знайти вектор c=2<a,b>с-3*b, де a,b,c, <x,y> – скалярний добуток векторів.
 """
-Ззнайти вектор
-c=2(a +c)- b, де a,b,c є R^n
+ dob_sk - скалярний добуток
+ sum - сумма
 """
-# 0.Позначення
-"""
-num - номер елемента вектору а 
-num_1 - номер елемента b
-num_2 - номер елемента c
-sum - сума всіх елментів вектору а
-sum_1 - сума всіх елментів вектору b
-sum_2 - сума всіх елментів вектору c
-"""
-n = int(input("Кількість елементів векторів: "))
+n = int(input('Введіть кількість елементів: '))
+sum = 0
+sum_1 = 0
+sum_2 = 0
+sum_3 = 0
 a = []
 b = []
 c = []
@@ -24,9 +21,11 @@ for i in range(n):
 for i in range(n):
     num_2 = int(input('Вектору c number #{0} = '.format(i)))
     c.append(num_2)
-i = 0
+for i in range(n):
+    dob_sk = a[i] * b[i]
+    sum += dob_sk
 for i in range(len(c)):
-    num_3 = (2 * (a[i] + c[i])) - b[i]
+    num_3 = (2 * sum * c[i]) - (3 * b[i])
     c.insert(i, num_3)
-    c.pop(i-1)
+    c.pop(i - 1)
 print("Вектор c = {0}".format(c))
